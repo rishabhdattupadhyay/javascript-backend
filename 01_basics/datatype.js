3ed462f08ebe0839243c23123870968014beafbd
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-04 19:32:53 +0000

    datatypes and variable in javascript

---
+++ 01_basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert(3+3) ; we are useing nodejs not browser 
+let name = "Rishabh"
+let age = 18;
+let isLogin = true;
+console.table([name,age]);
+/*number => range from 2 to the power 53
+bigint
+string = "";
+boolean = true/false;
+null = standalone value;
+undefiend = created a variable but not yet defiend;
+symbol = to identify unique things mostly used while react
+
+*/
+console.log(typeof "Rishi");
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 144434
+let Emailadress = "rishabh2002@gmail.com"
+var accountpasword = "Rishabh123"
+accountcity = "Prayagraj"
+//accountId = "112" not allowed to change the value of a constant if a variable is declared as constant
+console.log(accountId)
+Emailadress = "Rishi101"
+console.log(Emailadress)
+accountpassword = "Rishi12345"
+accountcity = "Agartala"
+let accountstate
+console.table([accountpassword,accountcity,Emailadress,accountId,accountstate])
