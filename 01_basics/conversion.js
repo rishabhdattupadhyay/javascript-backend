fc7058b95943ac421518e18723a5064256495898
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-05 07:57:59 +0000

    conversion between string number and boolean

---
+++ 01_basics/conversion.js
@@ -0,0 +1,19 @@
+let score = "33aab"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinNumber = Number(score)
+//console.log( typeof valueinNumber);
+//console.log(valueinNumber)
+/* '33'=> 33
+""=> NaN
+"33abc" =>NAN
+true = 1/false = 0*/
+let islogedin = 1
+ let booleancon = Boolean(islogedin);
+ console.log(booleancon);
+ //1 = true /0 = false /"" = false/"Rishabh" = true 
+ let numb = 33
+ let strcon = String(numb)
+ console.log(strcon);
+ console.log(typeof strcon);
