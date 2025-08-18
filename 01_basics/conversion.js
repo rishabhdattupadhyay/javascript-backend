ca14217bb13e947900c619b59f6edba866d2a0cc
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-18 06:20:54 +0000

    compareing diffrent datatype

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -9,11 +9,28 @@ let valueinNumber = Number(score)
 ""=> NaN
 "33abc" =>NAN
 true = 1/false = 0*/
-let islogedin = 1
- let booleancon = Boolean(islogedin);
- console.log(booleancon);
+//let islogedin = 1
+ //let booleancon = Boolean(islogedin);
+// console.log(booleancon);
  //1 = true /0 = false /"" = false/"Rishabh" = true 
- let numb = 33
- let strcon = String(numb)
- console.log(strcon);
- console.log(typeof strcon);
+// let numb = 33
+// let strcon = String(numb)
+// console.log(strcon);
+// console.log(typeof strcon);
+ let str = "Heloo"
+ let str2 = "  Rishi"
+ str3 = str + str2
+/* console.log(str3);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log("1" + "2"+ "2");
+ console.log(1+ 2+"2");*/
+ console.log(+true);
+ console.log(+"");
+
+ let num1 ,num2 , num3
+ num1 = num2 =num3 = 2+2
+
+let gamecounter = 100;
+gamecounter++
+console.log(gamecounter);
