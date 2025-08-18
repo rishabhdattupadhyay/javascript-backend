048bdc83e5dc67cb10d28157210c64ab2f982a86
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-18 19:08:31 +0000

    datatype comparison datatype summary

--- 01_basics/comp.js
+++ 01_basics/comp.js
@@ -7,4 +7,20 @@ console.log( "2" > 1)
 console.log( null > 0)
 console.log( null <= 0)
 console.log( null == 0)*/
-console.log( "2" == 2)
+//console.log( "2" == 2)
+const Id = Symbol("123")
+const anotherID = Symbol("123")
+console.log(Id === anotherID)
+// Refrence Non premetive data type  array object function
+const heros = ["Superman" , "Batman" , "Wolverien" , "Thor"]
+ console.log(heros)
+let info = {
+    name:"Rishabh",
+    age :22
+}
+ console.log(info)
+ const myfunction = function(){
+    console.log("Hello world")
+ }
+ console.log(myfunction)
+ console.log(typeof score)
