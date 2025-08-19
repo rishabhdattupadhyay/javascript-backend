041fa6e04bd60d72110ca783b6b63d9fba3193ec
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-19 07:41:14 +0000

    memory and type stack heap

---
+++ 01_basics/stackheap.js
@@ -0,0 +1,13 @@
+let myname = "Rishabh Datt Upadhyay"
+let anothername  = myname
+anothername = "Priya"
+console.log(myname)
+console.log(anothername)
+let userone = {
+    name:"Rishi",
+    upi:"700@upi"
+}
+let usertwo = userone
+usertwo.upi = "800@upi"
+console.log(userone);
+console.log(usertwo);
