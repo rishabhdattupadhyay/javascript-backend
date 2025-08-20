2e3a16f00cc1617e4f95ea6657e76a52db536458
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-20 07:48:57 +0000

    string and its proto

---
+++ 01_basics/string.js
@@ -0,0 +1,10 @@
+const name = "Rishabh"
+let repocount = 50
+console.log(name + repocount)
+ console.log(`name ${name} and repocount is ${repocount} `)
+ const gamename = new String("RishiFC")
+ console.log(gamename[0])
+ console.log(gamename.__proto__)
+
+ console.log();
+ 
