0485f9f4c48340c11b488e0a5fea9717708d19f6
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-22 07:50:15 +0000

    string and some operater that can perform function on string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -5,6 +5,16 @@ console.log(name + repocount)
  const gamename = new String("RishiFC")
  console.log(gamename[0])
  console.log(gamename.__proto__)
+ console.log(gamename.length);
+ console.log(gamename.toUpperCase());
+ const newgamename = gamename.substring(0,5)
+ console.log(newgamename);
+console.log(gamename.slice(-4,8));
+const newgameone = "     Rishi   "
+console.log(newgameone.trim());
+const url = "https://Rishabh%20Datt%20Upadhyay%20.com"
+console.log(url.replaceAll('%20' ,'_'));
+console.log(url.includes("https"));
 
- console.log();
+
  
