2e27ff73012883f9c0c5472bad57db6ddd9c42e6
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-26 08:10:55 +0000

    math function in js

---
+++ 01_basics/numbermath.js
@@ -0,0 +1,9 @@
+const score = 300
+console.log(score);
+const balance = new Number(100.044)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toPrecision(2));
+console.log(balance.toFixed(2));
+
+
