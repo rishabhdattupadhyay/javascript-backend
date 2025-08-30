cca6cc2bb8b423f9c7c79c0e71923a584f06c230
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-08-30 18:45:18 +0000

    Number and math function  useing Random to generate random value between min and max

--- 01_basics/numbermath.js
+++ 01_basics/numbermath.js
@@ -1,9 +1,30 @@
 const score = 300
-console.log(score);
+//console.log(score);
 const balance = new Number(100.044)
-console.log(balance)
-console.log(balance.toString().length);
-console.log(balance.toPrecision(2));
-console.log(balance.toFixed(2));
+//console.log(balance)
+//console.log(balance.toString().length);
+const othernum = 124.555
+//console.log(othernum.toPrecision(3));console.log(othernum.toFixed(2));
+//++++++++++++++++  MATH +++++++++
+/*console.log(Math);
+console.log(Math.round(4.5))
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2));
+console.log(Math.round(4.2));
+console.log(Math.max(4 , 5, 7, 8, 9));
+console.log(Math.min(4 , 5, 7, 8, 9)); */
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log((Math.random() * 10) + 1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
 
 
