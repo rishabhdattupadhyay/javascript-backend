e7b47cd9b4a88fef4945f2f8c8350a581d808038
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-09-01 07:46:30 +0000

    date use in javascript declaration.

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,13 @@
+const mydate = new Date()
+//console.log(mydate);
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());
+//console.log(mydate.toISOString());
+//console.log(mydate.toJSON());
+//console.log(mydate.toLocaleDateString());
+let newdate = new Date(2024 ,4,12)
+console.log(newdate.toString());
+
+
+
+
