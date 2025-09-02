9c8e681d129147ad14acb49f719d70a78048959a
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-09-02 12:05:47 +0000

    array declaration and diffrent functions slics splics push pop shift unshift

---
+++ 01_basics/02_basics/Array.js
@@ -0,0 +1,32 @@
+//Declaareing a array
+const Myarr=[1,2,3,4,5,true,"Rishi"]
+console.log(Myarr[1]);
+ const onepeice = ["Luffy","Zoro","Nami" , "Ussop","Samji", "Chopper"];
+console.log(onepeice[1]);
+const myarr  = new Array(1,2,3,4,5)
+console.log(myarr[4]);
+myarr.push(223)
+myarr.push(7)
+myarr.pop()
+myarr.unshift(9)
+myarr.unshift(12)
+myarr.shift()
+
+console.log(myarr.includes(9));
+console.log(myarr.indexOf(9));
+console.log(myarr);
+const newarr = myarr.join()
+console.log(newarr);
+console.log(myarr);
+let arr1 = myarr.slice(1,3)
+console.log("A",arr1);
+let arr2 = myarr.splice(1,3)
+console.log("B",arr2);
+console.log("c",myarr);
+
+
+
+
+
+ 
+
