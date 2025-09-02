eda62edd9c06849ed7ce34ff5ee45ca181913199
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-09-02 10:59:34 +0000

    using date and timestamp in javascript

--- 01_basics/dateandtime.js
+++ 01_basics/dateandtime.js
@@ -7,6 +7,25 @@ const mydate = new Date()
 //console.log(mydate.toLocaleDateString());
 let newdate = new Date(2024 ,4,12)
 console.log(newdate.toString());
+let createdate =new  Date("01-22-1999");
+console.log(createdate.toLocaleString());
+let mytimestamp = Date.now;
+console.log(mytimestamp);
+console.log(createdate.getTime());
+console.log(Math.floor(Date.now() / 1000));
+let datenew = new  Date()
+console.log(datenew.getMonth() + 1);
+console.log(Math.floor(datenew.getTime() + 1000));
+newdate.toLocaleString('default'{
+    weekday: "long"
+    
+})
+
+
+
+
+
+
 
 
 
