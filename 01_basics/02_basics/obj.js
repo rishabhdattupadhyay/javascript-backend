53c8732117381df639391e0f4afe59e3e574a3fc
Rishabh Datt Upadhyay <142716144+rishabhdattupadhyay@users.noreply.github.com>
2025-09-07 07:39:30 +0000

    object and function in object

--- 01_basics/02_basics/obj.js
+++ 01_basics/02_basics/obj.js
@@ -1,20 +1,58 @@
 //singleton
 //Object.create
-const currancy = Symbol("Key2")
+const mysymbol = Symbol("Key2")
 const Jsuser = {
     name:"Rishi",
     email:"rdupadhyay2002@gmail.com",
-    [mysym]: "mykey1",
-    "Full name" : "Rishabh Datt Upadhyay",
+    [mysymbol]: "mykey1",
+    Fullname : "Rishabh Datt Upadhyay",
     roll:52,
     intrest:"movies",
     isloggedin: false,
    lastlogedinday:["Monday","Friday"]
 }
-console.log(Jsuser["Full name"]);
-console.log( typeof Jsuser[currancy]);
-console.log(typeof currancy);
+/*console.log(Jsuser["Full name"]);
+console.log( typeof Jsuser[mysymbol]);
+console.log(typeof mysymbol);
 Jsuser.email = "2106052@kiit.ac.in";
 console.log(Jsuser.email);
+Object.freeze(Jsuser)
+Jsuser.email = "Rishabh@123gmail.com"
+console.log(Jsuser);*/
+Jsuser.greeting = Function();{
+   // console.log("Hello Js User ");
+}
+Jsuser.greetingTwo = Function();{
+   // console.log(`Hello JS user, ${Jsuser.name}`);
+}
+//console.log(Jsuser.greeting);
+//console.log(Jsuser.greetingTwo());
+//+++++++++++ Video 2 Object+++++++++++++++++++
+//const tinderuser = new Object() //singleton useing object constructor to create
+//console.log(tinderuser);
+//const tinderuser1 = {}
+//tinderuser1.name = "Rishi"
+//tinderuser1.id = "2213"
+//tinderuser1.loggedin = false
+//console.log(tinderuser1);
+const regularuser = {
+    email:"rdupadhyay2002@gmail.com",
+    gender:"Male",
+    fullname :{
+        userfullname :{
+        firstname:"Rishabh",
+        lastname:"Upadhyay"    }
+    }
+
+    }
+console.log(regularuser.fullname.userfullname.lastname);
+
+
+
+
+
+
+
+
 
 
